59bbc61449276066c428b4df66223a8f93cfd310
Saurabhy177 <17ysaurabh@gmail.com>
2025-10-11 23:11:03 +0530

    feat(onboarding): Add nutrient goal selection screen
    - Implements the UI for the `NutrientGoalScreen` to set carb, protein, and fat ratios.
    - The `NutrientGoalViewModel` handles input and uses the `ValidateNutrients` use case to ensure the total equals 100%.
    - A new DI module is added to provide the `ValidateNutrients` use case as a dependency.

--- app/src/main/java/com/example/calorietrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/calorietrackerapp/MainActivity.kt
@@ -18,6 +18,7 @@ import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
 import com.example.onboarding_presentation.goal.GoalTypeScreen
 import com.example.onboarding_presentation.height.HeightScreen
+import com.example.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.example.onboarding_presentation.weight.WeightScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -79,7 +80,10 @@ class MainActivity : ComponentActivity() {
                         }
 
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
 
                         composable(Route.TRACKER_OVERVIEW) {
