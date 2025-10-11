da4008a7a5673d1ec7aa18692a9a898b99d9066c
Saurabhy177 <17ysaurabh@gmail.com>
2025-10-11 14:08:05 +0530

    feat(onboarding): Add height and weight selection screens
    - Implements the UI and ViewModel for the `HeightScreen` and `WeightScreen`.
    - Each screen validates user input and uses its respective ViewModel to save the data to `Preferences`.
    - On successful input, navigates to the next step in the onboarding flow.

--- app/src/main/java/com/example/calorietrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/calorietrackerapp/MainActivity.kt
@@ -15,6 +15,8 @@ import com.example.calorietrackerapp.ui.theme.CalorieTrackerAppTheme
 import com.example.core.navigation.Route
 import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
+import com.example.onboarding_presentation.height.HeightScreen
+import com.example.onboarding_presentation.weight.WeightScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -53,11 +55,17 @@ class MainActivity : ComponentActivity() {
                         }
 
                         composable(Route.HEIGHT) {
-
+                            HeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
 
                         composable(Route.WEIGHT) {
-
+                            WeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
 
                         composable(Route.NUTRIENT_GOAL) {
